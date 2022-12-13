19d73a868ac9e1cdfb1157785e2abb0e6ae8af46
Javid A. Ismayilzada <javidai@code.local>
2022-12-13 21:28:25 +0400

    NewHomeWorkTask

---
+++ ObjectClass/ObjectClass/Confirmed.cs
@@ -0,0 +1,140 @@
+﻿
+#region ClassroomWork
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Confirmed
+    {
+        public string name;
+        public string surname;
+        public string address;
+
+    }
+}
+
+namespace ConsoleApp1
+{
+    internal class Confirmed2
+    {
+        public string name;
+        public string surname;
+        public string address;
+
+        public string FullConfirmed2()
+        {
+            return name + "-" + surname + "-" + address;
+        }
+
+    }
+}
+
+
+
+namespace ConsoleApp1
+{
+    internal class Confirmed3
+    {
+        public string name;
+        public string surname;
+        public string address;
+
+        public string FullConfirmed3()
+        {
+            return $"{name}, {surname}, {address}";
+        }
+
+    }
+}
+
+
+#endregion
+
+
+
+
+#region HomeWorkTask
+public class Account
+{
+    public string name;
+    public string password;
+
+    public Account(string name, string password)
+    {
+        this.name = name;
+        this.password = password;
+    }
+    public void Sign()
+    {
+        if (name == "Cavid123" && password == "Cavid1993")
+        {
+            Console.WriteLine("Ugurlu Giris");
+        }
+        else
+        {
+            Console.WriteLine("Username ve ya Password Dogru Deyil");
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+public class Account2
+{
+    public string name;
+    public string password;
+
+    public Account2(string name, string password)
+    {
+        this.name = name;
+        this.password = password;
+    }
+    public void Sign()
+    {
+        if (name == "Ibrahim" && password == "2110477")
+        {
+            Console.WriteLine("Ugurlu Giris");
+        }
+        else
+        {
+            Console.WriteLine("Username ve ya Password Dogru Deyil");
+        }
+    }
+
+
+
+}
+
+
+
+
+public class Factorial
+{
+    public int n;
+    public Factorial() { n = 0; }
+
+    public void NumsFactorial(long n)
+    {
+        long result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result = result * i;
+        }
+        Console.WriteLine(result);
+
+    }
+}
+
+#endregion
---
+++ ObjectClass/ObjectClass/Program.cs
@@ -0,0 +1,118 @@
+﻿
+
+#region ClassroomWork
+//using ConsoleApp1;
+
+//Confirmed confirmed = new Confirmed();
+
+//confirmed.name = "Shaiq";
+//confirmed.surname = "Kazimov";
+//confirmed.address = "Sedmoy";
+
+//Console.WriteLine($"{confirmed.name}-{confirmed.surname}-{confirmed.address}");
+
+
+
+
+//using ConsoleApp1;
+
+//Confirmed2 confirmed = new Confirmed2();
+
+//confirmed.name = "Shaiq";
+//confirmed.surname = "Kazimov";
+//confirmed.address = "Sedmoy";
+
+//Console.WriteLine(confirmed.FullConfirmed2());
+
+
+
+
+//using ConsoleApp1;
+
+//Confirmed3 confirmed = new Confirmed3();
+
+//confirmed.name = "Shaiq";
+//confirmed.surname = "Kazimov";
+//confirmed.address = "Sedmoy";
+
+//Console.WriteLine(confirmed.FullConfirmed3());
+
+
+
+
+//string name1 = "Cavid";
+//string surname1 = "Ismayilzade";
+//int age1 = 22;
+
+//Console.WriteLine($"{name1} {surname1} {age1}");
+
+
+
+//string name2 = "Shaiq";
+//string surname2 = "Kazimov";
+//int age2 = 25;
+
+//Console.WriteLine($"{name2} {surname2} {age2}");
+
+
+
+//string name3 = "Cavid";
+//string surname3 = "Bashirov";
+//int age3 = 29;
+
+//Console.WriteLine($"{name3} {surname3} {age3}");
+
+
+
+//var stu1 = new
+//{
+//    name = "cavid",
+//    surname = "ismayilzade",
+//    age = 22 
+//};
+
+//Console.WriteLine(stu1.name+ "-" +stu1.surname+ "-"+stu1.age);
+
+
+
+//var stu2 = new
+//{
+//    name = "cavid",
+//    surname = "ismayilzade",
+//    age = 22
+//};
+
+//Console.WriteLine(stu2.name + " " + stu2.surname + " " + stu2.age);
+
+
+#endregion
+
+
+
+
+#region HomeWork
+//Account sign1 = new Account("Cavid123", "Cavid1993");
+
+//sign1.Sign();
+
+
+
+
+
+//Account2 sign2 = new Account2("Ibrahim","211477");
+
+//sign2.Sign();
+
+
+
+
+
+//Factorial resault = new Factorial();
+//resault.NumsFactorial(10);
+
+
+
+
+#endregion
+
+
